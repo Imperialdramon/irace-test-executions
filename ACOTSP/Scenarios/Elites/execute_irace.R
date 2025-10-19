2dda76a718cf7e9f5a1d2ac54aa890bc14d90990
Imperialdramon <pabloelperez14@gmail.com>
2025-10-19 16:32:19 -0300

    Prepare elite executions

---
+++ ACOTSP/Scenarios/Elites/execute_irace.R
@@ -0,0 +1,7 @@
+library("irace")
+
+irace_cmdline("--only-test configurations.txt --scenario scenario.txt")
+# --test-instances-dir ../../../../TrainInstances
+# --test-instances-file test-instances.txt
+# --target-runner target-runner
+# --parameter-file parameters.txt
